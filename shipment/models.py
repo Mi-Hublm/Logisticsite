f93032aad5af87db46f16f292b9719f3bae5bc16
Ezekiel--dev <ezekielizuchi2018@gmail.com>
2023-10-09 17:11:14 +0100

    shipment models

--- shipment/models.py
+++ shipment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Shipment(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    quatity = models.IntegerField()
+    tracking_info = models.CharField(max_length=300)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name) + ' ' + str(self.quatity)
+    
+    class Meta:
+        ordering = ['date']
+        verbose_name = 'Shipment'
+        verbose_name_plural = 'Shipments'
