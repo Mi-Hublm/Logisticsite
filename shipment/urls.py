9bf25a9918f930be4ae1f1485cc18c414fb279c1
Ofiicalboss <mikeedaboss1@icloud.com>
2024-01-12 22:05:29 +0100

    fixed errors on the blog page

--- shipment/urls.py
+++ shipment/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from shipment import views
 
 urlpatterns = [
-    path("new_order", views.new_order, name="order"),
+    # path("new_order", views.new_order, name="order"),
     path("new_orders/", views.create_new_shipment, name="create"),
     path('cancel/orders/<str:id>/', views.cancel_shipment, name='cancel'),
     path('activate/orders/<str:id>/', views.activate_cancelled_shipment, name='activate'),
